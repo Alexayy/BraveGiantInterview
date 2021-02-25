9a6a21c31bf5ac26696292a4a9887d69a38bdddc
Alexayy <aleksa.cakic@gmail.com>
2021-02-26 00:57:43 +0100

    Logic fixed, TODO: Particle sys

---
+++ Assets/Scripts/SceneAndStateControllers/LevelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private int pointsToFinish;
+    private int currentPoints;
+    public GameObject pieces;
+
+    private void Start()
+    {
+        pointsToFinish = pieces.transform.childCount - 1;
+    }
+
+    public void AddPoints()
+    {
+        if (currentPoints >= pointsToFinish)
+        {
+            // TODO Particle Trigger here
+            SceneManager.LoadScene("SampleScene");
+            Debug.Log("All points: " + pointsToFinish);
+        }
+        
+        Debug.Log("Current points: " + currentPoints);
+        currentPoints++;
+    }
+}
